8820df2aed107a14bda7142d5a7a70b5b2da9555
grzegorzmikitowicz <grzegorz.mikitowicz-contractor@cognitran.com>
2023-03-20 07:17:17 +0100

    Two solutions started.

---
+++ src/main/java/com/itsgm/inprogress/hard/P126WordLadderII_bfs.java
@@ -0,0 +1,81 @@
+package com.itsgm.inprogress.hard;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class P126WordLadderII_bfs {
+
+    public static void main(String[] args) {
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = Arrays.asList("hot", "dot", "dog", "lot", "log");
+
+        Solution solution = new Solution();
+        List<List<String>> ladders = solution.findLadders(beginWord, endWord, wordList);
+        System.out.println(ladders);
+
+    }
+
+    static class Solution {
+
+        String endWord;
+        List<String> wordList;
+        byte[] wordIndices;
+
+        byte[][] cache;
+
+        public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+            return null;
+        }
+
+        public void bfs(Queue<LinkedList<Byte>> queue) {
+
+            LinkedList<Byte> positions;
+            int maxLength = Integer.MAX_VALUE;
+            boolean hasLengthLowerOrEqualToMaximal = true;
+
+            while (!queue.isEmpty()) {
+
+                positions = queue.remove();
+
+                if (difference(endWord, wordList.get(positions.getLast())) == 1) {
+
+                }
+
+
+                for (int i = 0; i < wordList.size(); i++) {
+                    if (!positions.contains(i)) {
+                        queue.add(copyWithElement(positions, i));
+                    }
+                }
+
+
+            }
+
+        }
+
+        LinkedList<Byte> copyWithElement(LinkedList<Byte> elements, Byte additionalElement) {
+            LinkedList<Byte> copy = new LinkedList<>(elements);
+            copy.add(additionalElement);
+            return copy;
+        }
+
+
+        public int difference(String s1, String s2) {
+            int diffCounter = 0;
+            for (int i = 0; i < s1.length(); i++) {
+                if (s1.charAt(i) != s2.charAt(i)) {
+                    diffCounter++;
+                }
+                if (diffCounter >= 2) {
+                    return -1;
+                }
+            }
+            return diffCounter;
+        }
+    }
+
+
+}
---
+++ src/main/java/com/itsgm/inprogress/hard/P1444NumberOfWaysOfCuttingAPizza.java
@@ -0,0 +1,36 @@
+package com.itsgm.inprogress.hard;
+
+public class P1444NumberOfWaysOfCuttingAPizza {
+
+    class Solution {
+        public int ways(String[] pizza, int k) {
+
+//            int ilen = pizza.length;
+//            int jlen = pizza[0].length();
+//            int result = 0;
+//
+//            /*
+//             *  1. whenever pizza is being cut vertically/horizontally, bottom-right corner of pizza remains in the same
+//             *  cell, that is [ilen-1][jlen-1], so if we approach the problem with dynamic programming and want to memoize
+//             *  results, it is enough to use 2d array instead of 4d
+//             */
+//            int[][] cache = new int[ilen][jlen];
+//
+//            int subresult = 1;
+//
+//
+//
+//            result = (result + subresult) % 1000000007;
+//
+//            return 0;
+
+            return 0;
+        }
+
+        public int ways(String[] pizza, int k, int iStart, int jStart) {
+            return 0;
+        }
+
+
+    }
+}
